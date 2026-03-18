4d0f6755977187beda468ab952bcffe69ee9c000
Matthias Bock <mail@matthiasbock.net>
2026-03-18 15:19:34 +0100

    Beginning tests for pofpack and pofunpack

---
+++ tests/pofpack/test.cpp
@@ -0,0 +1,18 @@
+
+#include <fstream>
+#include <string>
+#include <vector>
+#include <cstdint>
+
+
+// TODO: Include and test libalterapof
+
+
+int main(int argc, char** argv)
+{
+    if (argc != 2)
+    {
+        std::cerr << "Usage: " << argv[0] << " <pof file>" << std::endl;
+        return 1;
+    }
+}
